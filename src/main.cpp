bde7cb2a8b51e6e14e24e6514db1633a60b20639
ArminPP <app@gmx.at>
2021-01-25 22:24:16 +0100

    diplay background

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,11 @@ Alle anderen Libraries sind zu komplex und benötigen zu viel Speicher.
 #include <UI.h>
 #include <Arduino.h>
 
+
+
+// LCD Status
+bool LCD = true;
+
 void setup()
 {
   M5.begin();
@@ -27,8 +32,29 @@ void setup()
   UI_Draw_Footer("HOME", "GRAPH", "SETUP", 1, 0, 0);
 }
 
+
 void loop()
 {
   M5.update();
   UI_handleScreens(REFRESH_RATE);
+
+
+// DIM DISPLAY
+// https://m5stack.hackster.io/hague/covid-19-data-monitor-dfd267
+
+  if (M5.BtnA.pressedFor(2000))
+  {
+    if (LCD)
+    {
+      // M5.Lcd.writecommand(ST7735_DISPOFF);
+      M5.Lcd.setBrightness(0);
+      LCD = !LCD;
+    }
+    else
+    {
+      // M5.Lcd.writecommand(ST7735_DISPON);
+      M5.Lcd.setBrightness(100); //Brightness (0: Off - 255: Full)
+      LCD = !LCD;
+    }
+  }
 }
